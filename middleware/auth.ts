895b932c12db0ea33632ed6ad17b70524d2df459
Alex Hmelevcev <hmelevcev.aleksandr@sarkor.uz>
2024-07-08 17:10:32 +0500

    starting from scratch

--- middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { IncomingMessage, ServerResponse } from 'http';
-
-export const auth = (req: IncomingMessage, res: ServerResponse) => {
-  const token = jwt.sign({ user: 'user' }, 'secret', { expiresIn: '1h' });
-  res.setHeader('Authorization', `Bearer ${token}`);
-};
